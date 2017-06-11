e1b128448a70a49d112ca186b4d15740bc0090fa
Akko <dulo.rodrigo@gmail.com>
2017-06-11 14:14:32 +0000

    Oops

--- mpd/instalar.sh
+++ mpd/instalar.sh
@@ -6,10 +6,10 @@ mkdir -p ~/.config/mpd/playlists
 mkdir -p ~/.config/ncmpcpp
 echo "Creando las carpetas necesarias"
 
-cp mpd.conf ~/.config/mpd/playlists
+cp mpd.conf ~/.config/mpd/
 cp ncmpcpp.conf ~/.config/ncmpcpp/config
 echo "Copiando los archivos mpd.conf y ncmpcpp.conf"
 
-cd ~/.config/mpd
+cd ~/.config/mpd/
 touch {database,log,pid,state,sticker.sql}
 echo "Creando los archivos necesarios"
