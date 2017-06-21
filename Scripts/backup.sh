c45e5efafdbddb9abd4d84e2afcdde8594bb204f
Akko <dulo.rodrigo@gmail.com>
2017-06-21 02:38:36 +0000

    Mejor

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -2,10 +2,54 @@
 
 clear
 
-ls -d */
+mensajes(){
+    echo "¿Deseas respaldar todos los sitios? Sí (s) No (n)"
+    read todo
 
-echo "Elige el directorio del cual hacer el backup (sin incluir el /)"
-read directorio
+    if ( [ $todo == 's' ] || [ $todo == 'S' ] ); then
+        todo='S'
+    elif ( [ $todo == 'n' ] || [ $todo == 'N' ] ); then
+        todo='N'
+    else
+        error "!@"
+    fi
 
-tar -zcvf ${directorio}_$(date +%F).tar.gz $directorio
+    backup "!@"
+}
 
+backup(){
+    if [ $todo == 'S' ]; then
+        for i in *; do
+            if [ -d "${i}" ]; then
+                tar -zcvf ${i}_$(date +%F).tar.gz $i 
+            fi
+        done
+    else
+        ls -d */
+        echo "Selecciona un directorio del cual hacer respaldo (Sin incluir / )"
+        read directorio
+        tar -zcvf ${directorio}_$(date +%F).tar.gz $directorio
+        echo "¿Deseas continuar respaldando sitios? Sí (s) No (n)"
+        read continuar
+
+        if ( [ $continuar == 's' ] || [ $continuar == 'S' ] ); then
+            continuar='S'
+            directorio=''
+        elif ( [ $continuar == 'n' ] || [ $continuar == 'N' ] ); then
+            exit
+        else
+            error "!@"
+        fi
+
+        if [ $continuar == 'S' ]; then
+            backup "!@"
+        fi
+    fi
+}
+
+error(){
+    echo "Comando equivocado. Ejecuta de nuevo el script"
+    exit
+}
+
+mensajes "!@"
