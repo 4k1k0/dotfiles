cb5e616c13552a4f9a06e151d703c1fbd8c109ec
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2025-11-22 14:23:56 -0600

    gruvbox

--- nvim/lua/wako/colors.lua
+++ nvim/lua/wako/colors.lua
@@ -1,3 +1,27 @@
+--[[
+--
+-- These options are for the buffer tab.
+--
+
+  | Group                      | Meaning                             |
+  | -------------------------- | ----------------------------------- |
+  | `BufferLineFill`           | background behind the whole tabline |
+  | `BufferLineBackground`     | unselected buffers                  |
+  | `BufferLineBufferSelected` | active buffer                       |
+  | `BufferLineTabSelected`    | active tab                          |
+  | `BufferLineSeparator`      | separators                          |
+  | `BufferLineNumbers`        | buffer numbers                      |
+
+  For more highlight groups:
+  https://neovim.io/doc/user/builtin.html#synIDattr()
+
+  E.g.
+
+  vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "#1e1e1e" })
+
+--
+]]
+
 function ColorMyPencils(color)
 	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
@@ -8,4 +32,36 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, "CursorLine", { bg = "#3e4145" })
 end
 
-ColorMyPencils()
+function ColorGruv()
+  vim.o.background = "dark" -- or "light" for light mode
+
+  -- Default options:
+  require("gruvbox").setup({
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+  })
+
+  vim.cmd([[colorscheme gruvbox]])
+end
+
+-- ColorMyPencils()
+ColorGruv()
