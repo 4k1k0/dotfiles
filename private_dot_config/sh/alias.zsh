71ac197091c8b36f693ccd83ceae6a9e488c74f5
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2025-12-17 13:24:31 -0600

    adds sh helpers

---
+++ private_dot_config/sh/alias.zsh
@@ -0,0 +1,14 @@
+alias c="cd /Volumes/Nano/Code"
+alias n="cd /Volumes/Nano"
+alias cat="bat"
+alias gop="cd $GOPATH/src"
+alias htop="btm"
+alias images='wget -nd -H -p -A jpg,jpeg,png,gif -e robots=off'
+alias lc="tokei"
+alias ll="eza -l --icons --group-directories-first"
+alias ls="eza --icons --group-directories-first"
+alias unrar='unrar x'
+alias update="brew update && brew upgrade && mise plugin ls | xargs mise plugin upgrade"
+alias vim="nvim"
+alias kubectl="minikube kubectl --"
+alias mpd="nohup mpv http://192.168.0.3:8050/ > /dev/null 2>&1 &"
