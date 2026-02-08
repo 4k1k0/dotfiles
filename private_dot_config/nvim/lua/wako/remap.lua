96587647cbca52d43ba8480d09b340c4862571cd
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2026-02-07 20:17:04 -0600

    go to definitions

--- private_dot_config/nvim/lua/wako/remap.lua
+++ private_dot_config/nvim/lua/wako/remap.lua
@@ -11,3 +11,11 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+
+-- LSP key mappings
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
