ce909c0affe19d0fedef48456cb0db218517599a
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2026-05-01 22:02:55 -0600

    remove zsh file

--- private_dot_config/sh/functions.zsh
+++ private_dot_config/sh/functions.zsh
@@ -1,1 +1,11 @@
-pass() { export LC_CTYPE=C; export LANG=C; head /dev/urandom | tr -dc 'A-Za-z0-9_!#$%&/()=?+-' | head -c "$1" ; echo ''; }
+pass() { 
+  export LC_CTYPE=C; export LANG=C;
+  head /dev/urandom \
+  | tr -dc 'A-Za-z0-9_!#$%&/()=?+-' \
+  | head -c "$1" ; echo ''; 
+}
+
+kp() {
+  lsof -i :"$1" \
+  && kill -9 $(lsof -i :"$1")
+}
