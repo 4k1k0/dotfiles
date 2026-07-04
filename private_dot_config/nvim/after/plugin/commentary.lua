750989412611c40223f7a9b0a3a2e71f8cf14938
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2026-07-04 02:19:17 -0600

    comment options

--- private_dot_config/nvim/after/plugin/commentary.lua
+++
@@ -1,1 +0,0 @@
--- vim.keymap.set('n', '<leader>c', vim.cmd.Commentary)
--- private_dot_config/nvim/lua/wako/conform.lua
+++ private_dot_config/nvim/lua/wako/conform.lua
@@ -21,7 +21,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         {
           stdout_buffered = true,
           on_stdout = function (_, data)
-            print("out:", vim.inspect(data))
+            print "Check golangci-lint"
           end,
           on_stderr = function (_, _)
             print "Check golangci-lint"
